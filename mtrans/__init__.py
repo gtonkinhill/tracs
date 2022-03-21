111377161ecc15a73d4ee0ec38efe0c858e68562
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2022-03-21 07:42:24 +0100

    rename

--- mtrans/__init__.py
+++
@@ -1,3 +0,0 @@
-'''A fast python and and c++ pipeline for clustering sequneces using the 'transcluster' approach'''
-
-__version__ = '0.0.1'
--- mtrans/pairsnp.py
+++
@@ -1,8 +0,0 @@
-import os
-from MTRANS import pairsnp
-
-
-def run_pairsnp(msa, snp_threshold, outputfile, ncpu=1):
-    # runs pairsnp and reads result into a numpy array
-    
-    return None
