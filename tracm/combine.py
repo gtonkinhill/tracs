bc441fa21cbadd261d8500032e3131b72f7dd960
Gerry Tonkin-Hill <g.tonkinhill@gmail.com>
2023-04-20 10:22:21 +0200

    updated combine function

--- tracm/combine.py
+++ tracm/combine.py
@@ -5,6 +5,7 @@ import argparse
 import glob
 import gzip
 from collections import defaultdict
+from collections import ChainMap
 import pyfastx
 from joblib import Parallel, delayed
 
@@ -68,6 +69,26 @@ def find_ref(filename):
 
     return ref
 
+def sum_after_semicolon(line):
+    last_column = line.strip().split()[-1]
+    numbers_str = last_column.replace(':',',')
+    numbers = map(int, numbers_str.split(',')[2:])
+    return sum(numbers)
+
+def calculate_coverage(pileup):
+    sample = os.path.dirname(pileup).split(os.sep)[-1]
+    ref = re.search(r"ref_(.+)_pileup", os.path.basename(pileup)).group(1)
+
+    with gzip.open(pileup, "rt") as infile:
+        cov = 0
+        depth = 0
+        
+        for line in infile:
+            c = sum_after_semicolon(line)
+            if c>0 : cov += 1
+            depth += c
+
+    return (sample, ref, cov, depth)
 
 def combine(args):
     # check that all directories exist
@@ -94,17 +115,65 @@ def combine(args):
             ref = find_ref(aln)
             alignments[ref].append((sample, aln))
 
-    # write out as gzipped multifasta files
-    Parallel(n_jobs=args.n_cpu)(
+    # write out as gzipped multifasta files and calculate fraction of N's
+    ncovs = Parallel(n_jobs=args.n_cpu)(
         delayed(write_alignment)(ref, alns, args.output_dir, args.quiet)
             for ref, alns in alignments.items()
     )
+    ncovs = ChainMap(*ncovs)
+
+    print(ncovs.keys())
+
+    # calculate coverage
+    coverage = Parallel(n_jobs=args.n_cpu)(
+        delayed(calculate_coverage)(pileup)
+            for directory in args.directories
+            for pileup in glob.iglob(os.path.join(directory, "*_pileup.txt.gz"))
+    )
+
+    # merge and divide by length
+    coverage_dict = {}
+    for sample, ref, cov, depth in coverage:
+        if (sample, ref) in ncovs:
+            coverage_dict[(sample, ref)] = (cov/ncovs[(sample, ref)][1], depth/cov, depth/ncovs[(sample, ref)][1])
+        else:
+            coverage_dict[(sample, ref)] = ("NA", depth/cov, "NA")
+
+
+    # process sourmash results and write to output file
+    with open(args.output_dir + "combined_metadata.csv", "w") as outfile:
+        outfile.write("sample,intersect_bp,f_orig_query,f_match,f_unique_to_query,accession,coverage,mean_depth,mean_nonzero_depth,frac_N,species\n")
+        for directory in args.directories:
+            sample = os.path.basename(os.path.normpath(directory))
+            for sourmash in glob.iglob(os.path.join(directory, "*_sourmash_hits.csv")):
+                with open(sourmash, "r") as infile:
+                    next(infile) # skip header
+                    for line in infile:
+                        line = line.strip().split(",")
+                        accession = line[9].split()[0].strip('"')
+                        
+                        species = line[9].replace(accession, '').strip()
+                        
+                        if (sample, accession) in coverage_dict:
+                            cov = coverage_dict[(sample, accession)]
+                            if (sample, accession) in ncovs:
+                                ncov = str(ncovs[(sample, accession)][0])
+                            else:
+                                ncov = "NA"
+                            outfile.write(','.join([sample, accession] + line[:3] + [str(cov[0]), str(cov[1]), str(cov[2]), ncov, species]) + '\n')
+                        else:
+                            outfile.write(','.join([sample, accession] + line[:3] + ["NA", "NA", "NA", "NA",species]) + '\n')
+                        
 
     return
 
 
 def write_alignment(ref, alns, output_dir, quiet):
     output_file = output_dir + ref + "_combined.fasta.gz"
+    ncov = {}
+
+    if not quiet:
+        print("Writing combined alignment for {} to {}".format(ref, output_file))
 
     with gzip.open(output_file, "wt") as fasta_file:
         for aln in alns:
@@ -115,9 +184,9 @@ def write_alignment(ref, alns, output_dir, quiet):
                 if count > 1:
                     print("ERROR: {} contains more than one sequence".format(aln[1]))
                     sys.exit(1)
+                ncov[(aln[0], ref)] = (seq.count("N") / len(seq), len(seq))
 
-    if not quiet:
-        print("Writing combined alignment for {} to {}".format(ref, output_file))
+    return ncov
 
 
 def main():
