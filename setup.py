8000af59eefba94064f848496d38a66e2aa256e2
Sudaraka88 <sudarakm@Sudarakas-MacBook-Pro.local>
2024-07-12 18:17:06 +1000

    remove lomp from linux build

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if openmp and platform.system() == 'Darwin':
     extra_link_args.insert(-1, "-lomp")
 elif openmp:
     extra_compile_args.insert(-1, "-fopenmp")
-    extra_link_args.insert(-1, "-lomp")
+    # extra_link_args.insert(-1, "-lomp")
     # extra_compile_args.insert(-1, "-I/data1/gerryt/software/miniconda3/envs/mtrans/include")
     # extra_compile_args.insert(-1, "-L/data1/gerryt/software/miniconda3/envs/mtrans/lib")
     # extra_link_args.insert(-1, "-fopenmp")
